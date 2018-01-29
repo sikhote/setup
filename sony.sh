bf911f1e8c722b5aa3f0253d0fd80d648369583b
David Sinclair <24573542+sikhote@users.noreply.github.com>
2018-01-29 14:20:44 -0800

    Update sony.sh

--- sony.sh
+++ sony.sh
@@ -1,6 +1,9 @@
 echo 'base'
 chmod +x ./base.sh; ./base.sh
 
+echo 'git'
+git config --global user.email "david.sinclair@sony.com"
+
 echo 'desktop'
 yes | cp desktop.jpg ~/Pictures
 osascript -e 'tell application "System Events" to set picture of every desktop to ("/Users/1000315762/Pictures/desktop.jpg" as POSIX file as alias)'
