11659ff673fc382f29552dddab8705cce63565cd
David Sinclair <24573542+sikhote@users.noreply.github.com>
2019-07-21 11:43:14 -0700

    Update and rename base.sh to end.sh

--- end.sh
+++ end.sh
@@ -1,24 +1,4 @@
-echo 'brew'
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install git yarn awscli dark-mode zsh
-brew cask install dropbox
-brew cask install firefox
-brew cask install google-chrome
-brew cask install steam
-brew cask install the-unarchiver
-brew cask install vlc
-brew cask install spectacle
-brew cask install transmission
-brew cask install vscodium
-brew cask install now
-brew cask install iterm2
-brew cask install postman
-
-echo "projects"
-mkdir -p ~/Projects
-
 echo 'zsh'
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 yes | cp .zshrc ~/.zshrc
 yes | cp oxide.zsh-theme ~/.oh-my-zsh/custom/themes/oxide.zsh-theme
 
