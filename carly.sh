423cd6ce795ead380da530f2abd1bdac7d37367d
David Sinclair <24573542+sikhote@users.noreply.github.com>
2020-05-31 09:21:21 -0700

    Update carly.sh

--- carly.sh
+++ carly.sh
@@ -1,5 +1,11 @@
 echo 'base'
 chmod +x ./base.sh; ./base.sh
 
+echo 'computer'
+sudo scutil --set ComputerName "$USER"
+sudo scutil --set HostName "$USER"
+sudo scutil --set LocalHostName "$USER"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$USER"
+
 echo 'git'
 git config --global user.email "carlyhater@gmail.com"
