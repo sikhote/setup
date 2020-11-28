3b1684528dd05d9a1647ab10e667cd257dc46799
David Sinclair <24573542+sikhote@users.noreply.github.com>
2020-11-28 12:33:40 -0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -16,19 +16,19 @@ echo 'curl'
 sudo apt install -y curl
 
 echo 'dropbox'
-curl -o dropbox.deb -L https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2018.11.28_amd64.deb
+curl -o dropbox.deb -L https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb
 sudo dpkg -i ./dropbox.deb
 sudo apt -f install
 rm -rf dropbox.deb
 
-echo 'transmission'
-sudo apt-get install transmission -y
+# echo 'transmission'
+# sudo apt-get install transmission -y
 
-echo 'steam'
-curl -o steam.deb -L https://steamcdn-a.akamaihd.net/client/installer/steam.deb
-sudo dpkg -i ./steam.deb
-sudo apt -f install
-rm -rf steam.deb
+# echo 'steam'
+# curl -o steam.deb -L https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+# sudo dpkg -i ./steam.deb
+# sudo apt -f install
+# rm -rf steam.deb
 
 echo 'snap'
 sudo apt update
@@ -42,8 +42,7 @@ sudo snap install atom --classic
 sudo snap install chromium
 
 echo 'nvm & npm'
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source ~/.zshrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 nvm install 14
 nvm alias default 14
 npm install expo-cli --global
@@ -52,15 +51,6 @@ echo 'yarn'
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install --no-install-recommends yarn
-
-echo 'bash-git-prompt'
-cd ~/
-git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
-
-echo 'bash'
-cat ~/Downloads/setup-ubuntu-master/bash >> ~/.bashrc
-yes | cp ~/Downloads/setup-ubuntu-master/Solarized_Extravagant.bgptheme ~/.bash-git-prompt/themes/Solarized_Extravagant.bgptheme
-source ~/.bashrc
 
 echo 'desktop'
 cd ~/Downloads/setup-master/
