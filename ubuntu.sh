834273adfce28b106eb21281ef7bd8b171241192
David Sinclair <24573542+sikhote@users.noreply.github.com>
2020-11-28 10:31:06 -0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -27,7 +27,6 @@ echo 'atom'
 apm install --packages-file atom.txt
 
 echo 'git'
-sudo apt-get -y install git
 git config --global user.name "David Sinclair"
 git config --global user.email "david@sinclair.tech"
 
@@ -64,17 +63,13 @@ sudo snap install paintsupreme-3d
 echo 'nvm'
 sudo apt install -y curl
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-source ~/.bashrc
-nvm install 10
-nvm alias default 10
+nvm install 14
+nvm alias default 14
 
 echo 'yarn'
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install --no-install-recommends yarn
-
-echo "projects"
-mkdir -p ~/Projects
 
 echo 'bash-git-prompt'
 cd ~/
@@ -84,12 +79,6 @@ echo 'bash'
 cat ~/Downloads/setup-ubuntu-master/bash >> ~/.bashrc
 yes | cp ~/Downloads/setup-ubuntu-master/Solarized_Extravagant.bgptheme ~/.bash-git-prompt/themes/Solarized_Extravagant.bgptheme
 source ~/.bashrc
-
-echo 'fonts'
-cd ~/Downloads/setup-ubuntu-master
-mkdir -p ~/.fonts
-yes | cp fonts/* ~/.fonts/
-sudo fc-cache -fv
 
 echo 'desktop'
 cd ~/Downloads/setup-master/
