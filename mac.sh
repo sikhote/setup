36c03fb12c2f44d3011c128f0c81efe46eb0f636
David Sinclair <24573542+sikhote@users.noreply.github.com>
2021-01-19 08:26:30 -0800

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -21,7 +21,7 @@ brew cask install virtualbox
 brew cask install atom
 
 echo 'nvm & npm'
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 source ~/.zshrc
 nvm install 14
 nvm alias default 14
