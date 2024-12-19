b806d0ffea2ab84512bbda4ee1035a5ff743a734
David Sinclair <24573542+sikhote@users.noreply.github.com>
2024-12-19 12:33:55 -0800

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,5 +1,6 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+scoop install git
 
 scoop bucket add extras
 scoop install extras/fancontrol
