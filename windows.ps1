b3e65c7befb083b453a8086c708eb86888b3643a
David Sinclair <24573542+sikhote@users.noreply.github.com>
2024-12-19 17:22:56 -0800

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,25 +1,25 @@
-# Windows install instructions
+# Windows install
 # - Shift F10 once installer gets passed first reboot
-# type "oobe\bypassnro"
+# - type "oobe\bypassnro"
+#
+# Windows activate
+# - https://github.com/massgravel/Microsoft-Activation-Scripts
 
+# Scoop
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop install git
-
 scoop bucket add extras
 scoop install extras/fancontrol
 scoop install extras/vlc
 scoop install extras/firefox
 scoop install extras/hwmonitor
-
 scoop bucket add games
 scoop install games/steam
 scoop install games/epic-games-launcher
 scoop install games/battlenet
-
 scoop bucket add main
 scoop install main/7zip
 scoop install main/plex-server
-
 scoop bucket add nonportable
 scoop install nonportable/dropbox-np
